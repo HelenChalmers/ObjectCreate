3ea0a4846d200259f40690a769e6e341357047b2
Helen <helen@Helens-MacBook-Pro.local>
2018-07-29 21:00:03 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,39 @@
+console.log("Hey girl!");
+
+const Lwala = Object.create({}, {
+
+    business: {
+        value: "Lwala Community Alliance"
+    },
+    employmentStart: {
+        value: "01-01-2017"
+    },
+    employmentEnd: {
+        value: "07-01-2018"
+    },
+}),
+
+const Essential = Object.create({}, {
+    business: {
+        value: "Essential Audio"
+    },
+    employmentStart: {
+        value: "07-01-2016"
+
+    },
+    employmentEnd: {
+        value: "08-01-2017"
+    },
+}),
+
+const constributor = Object.create({}, {
+    business: {
+        value: "The Contributor"
+    },
+    employmentStart: {
+        value: "11-1-2013"
+    },
+    employmentEnd: {
+        value: "11-1-2014"
+    },
+})
